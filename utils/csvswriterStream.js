809bcb8ef93c7ef1da25a81bbbf54efe2a3766da
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2022-06-18 10:27:08 +1000

    Stream writer

---
+++ utils/csvswriterStream.js
@@ -0,0 +1,28 @@
+/*
+Author: Kripu Khadka
+Date: 18 June 2022
+File Description: Sync API file writer to the system
+
+*/
+
+
+
+const fs = require('fs');
+
+
+function streamWriter(pathLocation, fileName, data){
+  const streamWriter = fs.WriteStream(`${pathLocation}\\${fileName}.csv`);
+data.forEach((datax)=>{
+ streamWriter.write(`${datax},`);
+}) 
+
+  streamWriter.on("err", (err) => {
+    console.log(err);
+  });
+}
+
+
+
+streamWriter("./","Text.csv", ["aray", "kalay"])
+
+module.exports={streamWriter}
